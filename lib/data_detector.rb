741ae017c198910d3b555f22a38dfe1bfbaefb88
Luiz Paulo de Vasconcellos <luiz.pv9@gmail.com>
2014-12-04 23:59:11 -0200

    First version and tests for data_detector is working.

---
+++ lib/data_detector.rb
@@ -0,0 +1,54 @@
+class DataDetector
+	# Patterns used to check against string values to detect types
+	@@ipv4_pattern = /^(\d+)\.(\d+)\.(\d+)\.(\d+)$/
+	@@geolocation_pattern = /^geo\(\d+([\.,]\d+)?;\d+([\.,]\d+)?\)$/
+
+	class << self
+		# If the value is of a native json type, returns it. If not, returns nil
+		def detect_json_native_type(value)
+			return :boolean if value == true || value == false
+			return :number if value.is_a? Numeric
+			return :string if value.is_a? String
+		end
+
+		# Returns true if the specified value has a valid format of a ipv4 address
+		def is_ipv4_address(value)
+			return false unless value.is_a? String
+			match = value.match @@ipv4_pattern
+			match && 
+				match[1].to_i <= 255 && 
+				match[2].to_i <= 255 && 
+				match[3].to_i <= 255 && 
+				match[4].to_i <= 255
+		end
+
+		# Detect type of the specified value.  Current possible values are:
+		# * boolean - true | false (native json format)
+		# * number - float64 (native json format)
+		# * string - any size (native json format)
+		# * array
+		# * ip
+		# * geolocation
+		#
+		# There was also planning for 
+		# * Timestamp - Removed because it will be stored as a number, and the filter
+		#               can be configured to be a datepicker (and every date in the
+		#               system is stored as a number (timestamp).
+		# * DateTime - Removed in order to create a standard format (timestamp).
+		# * Currency - Removed because it would be stored as a regular number,
+		#              and aggregation functions will be provided for all numeric
+		#              values, not just currency.
+		#		
+		def detect(value)
+			json_native_type = detect_json_native_type value
+			if json_native_type && json_native_type != :string
+				return json_native_type
+			end
+
+			return :array if value.is_a? Array
+			return :ip if is_ipv4_address value
+			return :geolocation if value.match @@geolocation_pattern
+			return :string
+		end
+	end
+end
---
+++ spec/unit/data_detector_spec.rb
@@ -0,0 +1,83 @@
+require 'rails_helper'
+require 'data_detector'
+
+describe 'DataDetector specs' do
+	describe '.detect_native_json_type' do
+		it 'detects boolean type' do
+			expect(DataDetector.detect_json_native_type(true)).to eq :boolean
+			expect(DataDetector.detect_json_native_type(false)).to eq :boolean
+		end
+
+		it 'detects number type' do
+			expect(DataDetector.detect_json_native_type(1)).to eq :number
+			expect(DataDetector.detect_json_native_type(1.412)).to eq :number
+			expect(DataDetector.detect_json_native_type(0)).to eq :number
+		end
+
+		it 'detects string type' do
+			expect(DataDetector.detect_json_native_type('what')).to eq :string
+			expect(DataDetector.detect_json_native_type('foo, right?')).to eq :string
+			expect(DataDetector.detect_json_native_type('')).to eq :string
+		end
+
+		it 'doesnt detect array type' do
+			expect(DataDetector.detect_json_native_type([])).to eq nil
+			expect(DataDetector.detect_json_native_type(['right'])).to eq nil
+		end
+
+		it 'doesnt detect object type' do
+			expect(DataDetector.detect_json_native_type({})).to eq nil
+			expect(DataDetector.detect_json_native_type({'a' => 10})).to eq nil
+		end
+	end
+
+	describe '.is_ipv4_address' do
+		it 'returns true for valid address' do
+			expect(DataDetector.is_ipv4_address('10.7.16.69')).to be true
+			expect(DataDetector.is_ipv4_address('192.0.0.1')).to be true
+		end
+
+		it 'returns false for invalid address' do
+			expect(DataDetector.is_ipv4_address('10.7.16.69.15')).to be_falsy
+			expect(DataDetector.is_ipv4_address('266.12.10.0')).to be_falsy
+		end
+
+		it 'handles gracefully non string values' do
+			expect(DataDetector.is_ipv4_address([])).to be_falsy
+			expect(DataDetector.is_ipv4_address(nil)).to be_falsy
+			expect(DataDetector.is_ipv4_address(205)).to be_falsy
+		end	
+	end
+
+	describe '.detect' do
+		it 'detects json native boolean' do
+			expect(DataDetector.detect(true)).to eq :boolean
+			expect(DataDetector.detect(false)).to eq :boolean
+		end
+
+		it 'detects json native number' do
+			expect(DataDetector.detect(12.51)).to eq :number
+			expect(DataDetector.detect(0)).to eq :number
+		end
+
+		it 'detects json native string' do
+			expect(DataDetector.detect('12.51')).to eq :string
+			expect(DataDetector.detect('0')).to eq :string
+		end
+
+		it 'detects ip type' do
+			expect(DataDetector.detect('10.7.16.69')).to be :ip
+			expect(DataDetector.detect('192.0.0.1')).to be :ip
+		end
+
+		it 'detects geolocation type' do
+			expect(DataDetector.detect('geo(10;15)')).to be :geolocation
+			expect(DataDetector.detect('geo(23.2566;90.5111)')).to be :geolocation
+		end
+
+		it 'detects array type' do
+			expect(DataDetector.detect([1, 2])).to be :array
+			expect(DataDetector.detect(['a', 'b'])).to be :array
+		end
+	end
+end
