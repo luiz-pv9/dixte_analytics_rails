23bd217d4085280ec60c9974a54616e7ddef904f
Luiz Paulo de Vasconcellos <luiz.pv9@gmail.com>
2015-01-06 06:39:05 -0200

    Developing EventTracker

--- app/workers/event_tracker.rb
+++ app/workers/event_tracker.rb
@@ -1,6 +1,21 @@
+require 'data_cleaner'
+
 class EventTracker
 	include Sidekiq::Worker
 
 	def perform(data)
+		cleaned = clean(data)
+		if cleaned.size == data.size
+			# Store
+		else
+			# Generate error
+		end
+	end
+
+	def clean(data)
+		DataCleaner.clean_hash(data, [
+			:json_simple_value,
+			[:json_string_value]
+		])
 	end
 end
--- spec/workers/event_tracker_spec.rb
+++ spec/workers/event_tracker_spec.rb
@@ -1,4 +1,8 @@
 require 'rails_helper'
 
 describe EventTracker do
+	describe 'cleaning the event'
+	describe 'warning generation on bad formatted events'
+	describe 'appending the profile properties in the events'
+	describe 'storing the event'
 end
