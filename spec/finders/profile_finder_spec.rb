021cbc10e73034b16e6b93c80cdf511624ccd6fb
Luiz Paulo de Vasconcellos <luiz.pv9@gmail.com>
2015-01-07 10:48:08 -0200

    Begining tests of profile finder

--- spec/finders/profile_finder_spec.rb
+++ spec/finders/profile_finder_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
+require 'collections'
 
 describe ProfileFinder do
 
 	before :each do
+		@profiles = Collections::Profiles.collection
 		@profile_tracker = ProfileTracker.new
+
+		@profiles.find.remove_all
 	end
 
 	def valid_app
@@ -21,12 +25,25 @@ describe ProfileFinder do
 		})
 	end
 
-	describe 'finding by external_id' do
+	describe '.by_external_id' do
 		it 'finds a profile by the specified external_id' do
 			profile = track_profile('lpvasco')
 			found = ProfileFinder.by_external_id('lpvasco')
 			expect(found).to be_truthy
 			expect(found['external_id']).to eq('lpvasco')
 		end
+
+		it 'returns nil if no profile is found' do
+			profile = ProfileFinder.by_external_id('lpvasco')
+			expect(profile).to be_nil
+		end
+	end
+
+	describe '.by_properties' do
+		def store_profiles
+			app = 
+		end
+		it 'finds profiles by the properties they have'
+		it 'cleans bad formatted queries'
 	end
 end
