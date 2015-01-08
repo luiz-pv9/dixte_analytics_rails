8d8391e39654e692876b34f4ecb4b0c04c911eb8
Luiz Paulo de Vasconcellos <luiz.pv9@gmail.com>
2015-01-08 03:50:10 -0200

    ProfileUntracker tests are passing

---
+++ app/workers/profile_untracker.rb
@@ -0,0 +1,36 @@
+require 'collections'
+
+class ProfileUntracker
+	include Sidekiq::Worker
+	@@collection = Collections::Profiles.collection
+
+	def untrack_properties(profile)
+		if profile['properties'] && profile['properties'].size > 0
+			property_untracker = PropertyUntracker.new(
+				App.profile_properties_key(profile['app_token']), profile['properties'])
+			property_untracker.untrack!
+		end
+	end
+
+	def remove_profile(profile)
+		@@collection.find('_id' => profile['_id']).remove
+	end
+
+	def untrack_profile(app_token, external_id)
+		profile = ProfileFinder.by_external_id(app_token, external_id)
+		if profile
+			untrack_properties(profile)
+			remove_profile(profile)
+		end
+	end
+
+	def perform(app_token, external_id)
+		if external_id.is_a?(Array)
+			external_id.each do |e_external_id|
+				untrack_profile(app_token, e_external_id)
+			end
+		else
+			untrack_profile(app_token, external_id)
+		end
+	end
+end
---
+++ spec/workers/profile_untracker_spec.rb
@@ -0,0 +1,107 @@
+require 'rails_helper'
+require 'collections'
+
+describe ProfileUntracker do
+	before :each do
+		@profiles = Collections::Profiles.collection
+		@properties = Collections::Properties.collection
+		Collections::Profiles.collection.find.remove_all
+		Collections::Properties.collection.find.remove_all
+		@profile_tracker = ProfileTracker.new
+		@profile_untracker = ProfileUntracker.new
+		@app = App.create :name => 'Dixte'
+	end
+
+	def track_profile(external_id, properties = {})
+		@profile_tracker.perform({
+			'app_token' => @app.token,
+			'external_id' => external_id,
+			'properties' => properties
+		})
+	end
+
+	def track_profiles_1
+		track_profile('lpvasco', {
+			'account type' => 'premium',
+			'platform' => 'ios'
+		})
+		track_profile('luiz', {
+			'account type' => 'normal',
+			'platform' => 'web'
+		})
+	end
+
+	describe 'untracking a profile' do
+		it 'removes the profile from the external_id' do
+			track_profiles_1
+			expect {
+				@profile_untracker.perform(@app.token, 'lpvasco')
+			}.to change { @profiles.find.count }.by(-1)
+		end
+
+		it 'untracks the properties in the profile being removed' do
+			track_profiles_1
+			@profile_untracker.perform(@app.token, 'lpvasco')
+			property = @properties.find.first
+			expect(property.except('_id')).to eq({
+				'key' => @app.token + '#profiles',
+				'properties' => {
+					'account type' => {
+						'type' => 'string',
+						'values' => {
+							'normal' => 1
+						}
+					},
+					'platform' => {
+						'type' => 'string',
+						'values' => {
+							'web' => 1
+						}
+					}
+				}				
+			})
+		end
+	end
+
+	def track_profiles_2
+		track_profile('lpvasco', {
+			'account type' => 'premium',
+			'platform' => 'ios'
+		})
+		track_profile('luiz', {
+			'account type' => 'normal',
+			'platform' => 'web'
+		})
+		track_profile('sinxoll', {
+			'account type' => 'premium',
+			'platform' => 'android'
+		})
+	end
+
+	describe 'untracking multiple profiles' do
+		it 'removes multiple profiles from external ids' do
+			track_profiles_2
+			expect {
+				@profile_untracker.perform(@app.token, ['lpvasco', 'sinxoll'])
+			}.to change { @profiles.find.count }.by(-2)
+			property = @properties.find.first
+			expect(property.except('_id')).to eq({
+				'key' => @app.token + '#profiles',
+				'properties' => {
+					'account type' => {
+						'type' => 'string',
+						'values' => {
+							'normal' => 1
+						}
+					},
+					'platform' => {
+						'type' => 'string',
+						'values' => {
+							'web' => 1
+						}
+					}
+				}				
+			})
+		end
+	end
+end
