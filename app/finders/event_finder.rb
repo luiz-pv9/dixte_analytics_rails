91efce681b67b539df4818e6b8a0ac35a101a2b3
Luiz Paulo de Vasconcellos <luiz.pv9@gmail.com>
2015-01-08 10:37:12 -0200

    Tests for finding events by properties and TimeRange integration are passing

--- app/finders/event_finder.rb
+++ app/finders/event_finder.rb
@@ -12,5 +12,21 @@ class EventFinder
 			time_range.append_to_query('happened_at', query)
 			@@collection.find(query)
 		end
+
+		def by_type_and_properties(app_token, type, properties, time_range = nil)
+			time_range = TimeRange.new unless time_range
+			properties = DataCleaner.clean_hash(properties, [
+				:json_simple_value,
+				{'$gt' => :json_numeric_value},
+				{'$lt' => :json_numeric_value},
+				{'$in' => [:json_simple_value]}
+			])
+			query = {'app_token' => app_token}
+			properties.each do |key, val|
+				query["properties.#{key}"] = val
+			end
+			time_range.append_to_query('happened_at', query)
+			@@collection.find(query)
+		end
 	end
 end
--- spec/finders/event_finder_spec.rb
+++ spec/finders/event_finder_spec.rb
@@ -64,18 +64,78 @@ describe EventFinder do
 	end
 
 	def track_events_2
-		track_event('click button', {'label' => 'help', 'size' => 2})
-		track_event('click button', {'label' => 'what', 'size' => 1})
-		track_event('open modal', {'title' => 'title 1'})
-		track_event('open modal', {'title' => 'title 2'})
-		track_event('open modal', {'title' => 'title 3'})
+		track_event('click button', {'label' => 'help', 'size' => 2, 'colors' => ['red', 'blue']})
+		track_event('click button', {'label' => 'what', 'size' => 1, 'colors' => ['blue', 'yellow']})
+		track_event('open modal', {'title' => 'title 1', 'type' => 1})
+		track_event('open modal', {'title' => 'title 2', 'type' => 2})
+		track_event('open modal', {'title' => 'title 3', 'type' => 2})
+	end
+
+	def track_events_2_time
+		track_event_at_time('click button', {'label' => 'help', 'size' => 2, 'colors' => ['red', 'blue']}, 1000)
+		track_event_at_time('click button', {'label' => 'what', 'size' => 1, 'colors' => ['blue', 'yellow']}, 1001)
+		track_event_at_time('open modal', {'title' => 'title 1', 'type' => 1}, 1002)
+		track_event_at_time('open modal', {'title' => 'title 2', 'type' => 2}, 1003)
+		track_event_at_time('open modal', {'title' => 'title 3', 'type' => 2}, 1004)
 	end
 
 	describe '.by_type_with_properties' do
 		it 'finds events by type and properties' do
 			track_events_2
-			# events = EventFinder.by_type_with_properties(@app.token, 'click button', {
-			# })
+			events = EventFinder.by_type_and_properties(@app.token, 'click button', {
+				'label' => 'what'
+			})
+			expect(events.count).to eq(1)
+
+			events = EventFinder.by_type_and_properties(@app.token, 'open modal', {
+				'type' => 2
+			})
+			expect(events.count).to eq(2)
+		end
+
+		it 'finds events by proeprty simple value' do
+			track_events_2
+			events = EventFinder.by_type_and_properties(@app.token, 'click button', {
+				'size' => 1
+			})
+			expect(events.count).to eq(1)
+		end
+
+		it 'finds events by property numeric value with $gt operator' do
+			track_events_2
+			events = EventFinder.by_type_and_properties(@app.token, 'click button', {
+				'size' => {'$gt' => 1}
+			})
+			expect(events.count).to eq(1)
+		end
+
+		it 'finds events by property numeric value with $lt operator' do
+			track_events_2
+			events = EventFinder.by_type_and_properties(@app.token, 'click button', {
+				'size' => {'$lt' => 2}
+			})
+			expect(events.count).to eq(1)
+		end
+
+		it 'finds events by array property with $in operator' do
+			track_events_2
+			events = EventFinder.by_type_and_properties(@app.token, 'click button', {
+				'colors' => {'$in' => ['blue']}
+			})
+			expect(events.count).to eq(2)
+
+			events = EventFinder.by_type_and_properties(@app.token, 'click button', {
+				'colors' => {'$in' => ['red']}
+			})
+			expect(events.count).to eq(1)
+		end
+
+		it 'finds events by type and properties and time range' do
+			track_events_2_time
+			events = EventFinder.by_type_and_properties(@app.token, 'click button', {
+				'colors' => {'$in' => ['blue']}
+			}, TimeRange.new(999, 1000))
+			expect(events.count).to eq(1)
 		end
 	end
 end
