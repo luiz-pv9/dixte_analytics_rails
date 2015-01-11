59b841ef4910f41474585df905afd492234bd303
Luiz Paulo de Vasconcellos <luiz.pv9@gmail.com>
2015-01-11 18:30:16 -0200

    First tests for segmentation report are passing

--- app/reports/segmentation_report.rb
+++ app/reports/segmentation_report.rb
@@ -28,14 +28,87 @@ class SegmentationReport < ApplicationReport
 		@report['steps'] = @time_range.steps_in(@config['steps_in'])
 	end
 
-	def detect_segments
-		@report['series'] = {}
+	def detect_segments_total
 		@events.each do |event|
-			event_property = event['properties'][@config['segment_on']]
+			if @config['segment_on']
+				event_property = event['properties'][@config['segment_on']]
+			else
+				event_property = @config['event_type']
+			end
+			event_property ||= 'null'
 			@report['series'][event_property] ||= []
 			index = closest_value_index(@report['steps'], event['happened_at'])
 			@report['series'][event_property][index] ||= 0
 			@report['series'][event_property][index] += 1
+		end
+	end
+
+	def detect_segments_average
+		pre_series = {}
+		@events.each do |event|
+			if @config['segment_on']
+				event_property = event['properties'][@config['segment_on']]
+			else
+				event_property = @config['event_type']
+			end
+			event_property ||= 'null'
+
+			pre_series[event_property] ||= []
+			@report['series'][event_property] ||= []
+
+			index = closest_value_index(@report['steps'], event['happened_at'])
+			pre_series[event_property][index] ||= []
+
+			if pre_series[event_property][index].index(event['external_id']) == nil
+				pre_series[event_property][index] << event['external_id']
+			end
+
+			@report['series'][event_property][index] ||= 0
+			@report['series'][event_property][index] += 1
+		end
+
+		pre_series.each do |key, val|
+			average_series = @report['series'][key].zip(val)
+			@report['series'][key] = average_series.map do |con|
+				con[0] / con[1].size
+			end
+		end
+	end
+
+	def detect_segments_unique
+		pre_series = {}
+		@events.each do |event|
+			if @config['segment_on']
+				event_property = event['properties'][@config['segment_on']]
+			else
+				event_property = @config['event_type']
+			end
+			event_property ||= 'null'
+			pre_series[event_property] ||= []
+
+			index = closest_value_index(@report['steps'], event['happened_at'])
+
+			pre_series[event_property][index] ||= []
+			if pre_series[event_property][index].index(event['external_id']) == nil
+				pre_series[event_property][index] << event['external_id']
+			end
+		end
+
+		pre_series.each do |key, val|
+			@report['series'][key] = val.map { |v| v.size }
+		end
+	end
+
+	def detect_segments
+		@report['series'] = {}
+		@config['grouping'] ||= 'total'
+		case @config['grouping']
+		when 'total'
+			detect_segments_total
+		when 'unique'
+			detect_segments_unique
+		when 'average'
+			detect_segments_average
 		end
 	end
 
--- spec/reports/segmentation_report_spec.rb
+++ spec/reports/segmentation_report_spec.rb
@@ -14,6 +14,7 @@ describe SegmentationReport do
 		@event_tracker.perform({
 			'app_token' => app_token,
 			'external_id' => external_id,
+			'happened_at' => time.to_i,
 			'type' => type,
 			'properties' => properties
 		})
@@ -25,6 +26,37 @@ describe SegmentationReport do
 		track_event('click button', '03/01/2014', {'label' => 'what'})
 		track_event('click button', '04/01/2014', {'label' => 'what'})
 		track_event('click button', '05/01/2014', {'label' => 'what'})
+	end
+
+	def track_data_2
+		track_event('click button', '01/01/2014', {'label' => 'what'})
+		track_event('click button', '02/01/2014', {'label' => 'foo'})
+		track_event('click button', '03/01/2014', {})
+		track_event('click button', '04/01/2014', {'label' => 'what'})
+		track_event('click button', '05/01/2014', {'label' => 'what'})
+	end
+
+	def track_data_3
+		track_event('click button', '01/01/2014', {'label' => 'what'}, 'lpvasco')
+		track_event('click button', '01/01/2014', {'label' => 'what'}, 'lpvasco')
+		track_event('click button', '02/01/2014', {'label' => 'what'}, 'lpvasco')
+		track_event('click button', '03/01/2014', {'label' => 'what'}, 'lpvasco')
+		track_event('click button', '04/01/2014', {'label' => 'what'}, 'lpvasco')
+		track_event('click button', '05/01/2014', {'label' => 'what'}, 'lpvasco')
+
+		track_event('click button', '01/01/2014', {'label' => 'what'}, 'fran')
+		track_event('click button', '01/01/2014', {'label' => 'what'}, 'fran')
+		track_event('click button', '02/01/2014', {'label' => 'what'}, 'fran')
+		track_event('click button', '03/01/2014', {'label' => 'what'}, 'fran')
+		track_event('click button', '04/01/2014', {'label' => 'what'}, 'fran')
+		track_event('click button', '05/01/2014', {'label' => 'what'}, 'fran')
+
+		track_event('click button', '01/01/2014', {'label' => 'what'}, 'luizpv9')
+		track_event('click button', '01/01/2014', {'label' => 'what'}, 'luizpv9')
+		track_event('click button', '02/01/2014', {'label' => 'what'}, 'luizpv9')
+		track_event('click button', '03/01/2014', {'label' => 'what'}, 'luizpv9')
+		track_event('click button', '04/01/2014', {'label' => 'what'}, 'luizpv9')
+		track_event('click button', '05/01/2014', {'label' => 'what'}, 'luizpv9')
 	end
 
 
@@ -44,10 +76,105 @@ describe SegmentationReport do
 
 			expect(report['steps'].size).to eq(5)
 			expect(report['series'].size).to eq(1)
+			expect(report['series']['what']).to eq([1, 1, 1, 1, 1])
 		end
 
-		it 'generates a segmentation on the total of event if no property to segment is provided'
-		it 'includes a null value for the events that doesnt have the property'
+		it 'generates a segmentation on the total of event if no property to segment is provided' do
+			track_data_1
+			report = SegmentationReport.new({
+				'app_token' => @app.token,
+				'event_type' => 'click button',
+				'segment_on' => nil,
+				'steps_in' => 'days',
+				'time_range' => {
+					'from' => Time.strptime('01/01/2014', '%d/%m/%Y').to_i,
+					'to' => Time.strptime('05/01/2014', '%d/%m/%Y').to_i
+				}
+			}).to_json
+
+			expect(report['steps'].size).to eq(5)
+			expect(report['series'].size).to eq(1)
+			expect(report['series']['click button']).to eq([1, 1, 1, 1, 1])
+		end
+
+		it 'includes a null value for the events that doesnt have the property' do
+			track_data_2
+			report = SegmentationReport.new({
+				'app_token' => @app.token,
+				'event_type' => 'click button',
+				'segment_on' => 'label',
+				'steps_in' => 'days',
+				'time_range' => {
+					'from' => Time.strptime('01/01/2014', '%d/%m/%Y').to_i,
+					'to' => Time.strptime('05/01/2014', '%d/%m/%Y').to_i
+				}
+			}).to_json
+
+			expect(report['steps'].size).to eq(5)
+			expect(report['series'].size).to eq(3)
+			expect(report['series']['what']).to eq([1, nil, nil, 1, 1])
+			expect(report['series']['foo']).to eq([nil, 1])
+			expect(report['series']['null']).to eq([nil, nil, 1])
+		end
+	end
+
+	describe 'grouping' do
+		it 'generates the segmentation of the total properties' do
+			track_data_3
+			report = SegmentationReport.new({
+				'app_token' => @app.token,
+				'event_type' => 'click button',
+				'segment_on' => 'label',
+				'steps_in' => 'days',
+				'grouping' => 'total',
+				'time_range' => {
+					'from' => Time.strptime('01/01/2014', '%d/%m/%Y').to_i,
+					'to' => Time.strptime('05/01/2014', '%d/%m/%Y').to_i
+				}
+			}).to_json
+
+			expect(report['steps'].size).to eq(5)
+			expect(report['series'].size).to eq(1)
+			expect(report['series']['what']).to eq([6, 3, 3, 3, 3])
+		end
+
+		it 'generates the segmentation uniquely per profile' do
+			track_data_3
+			report = SegmentationReport.new({
+				'app_token' => @app.token,
+				'event_type' => 'click button',
+				'segment_on' => 'label',
+				'steps_in' => 'days',
+				'grouping' => 'unique',
+				'time_range' => {
+					'from' => Time.strptime('01/01/2014', '%d/%m/%Y').to_i,
+					'to' => Time.strptime('05/01/2014', '%d/%m/%Y').to_i
+				}
+			}).to_json
+
+			expect(report['steps'].size).to eq(5)
+			expect(report['series'].size).to eq(1)
+			expect(report['series']['what']).to eq([3, 3, 3, 3, 3])
+		end
+
+		it 'generates the segmentation of the average per profile' do
+			track_data_3
+			report = SegmentationReport.new({
+				'app_token' => @app.token,
+				'event_type' => 'click button',
+				'segment_on' => 'label',
+				'steps_in' => 'days',
+				'grouping' => 'average',
+				'time_range' => {
+					'from' => Time.strptime('01/01/2014', '%d/%m/%Y').to_i,
+					'to' => Time.strptime('05/01/2014', '%d/%m/%Y').to_i
+				}
+			}).to_json
+
+			expect(report['steps'].size).to eq(5)
+			expect(report['series'].size).to eq(1)
+			expect(report['series']['what']).to eq([2, 1, 1, 1, 1])
+		end
 	end
 
 	describe 'time interval' do
@@ -59,11 +186,4 @@ describe SegmentationReport do
 		it 'generates the report on semester interval'
 		it 'generates the report on year interval'
 	end
-
-	describe 'grouping' do
-		it 'generates the segmentation of the total properties'
-		it 'generates the segmentation of the average per profile'
-		it 'generates the segmentation uniquely per profile'
-	end
-
 end
